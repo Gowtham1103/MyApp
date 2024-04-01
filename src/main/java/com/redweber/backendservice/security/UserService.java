6050cd90e46bf188b8aa9d6cbfacfdc19761e00c
Gowtham1103 <gowtham19.bm@gmail.com>
2024-04-01 15:10:07 +0530

    security updated

--- src/main/java/com/redweber/backendservice/security/UserService.java
+++ src/main/java/com/redweber/backendservice/security/UserService.java
@@ -1,15 +1,43 @@
 package com.redweber.backendservice.security;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
+
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
 
-    public static boolean isValidPassword(String password) {
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public ResponseEntity<?> signUp(User credentials) {
+        if (userRepository.existsByUsername(credentials.getUsername())) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Username not available!");
+        }
+        if (!isValidPassword(credentials.getPassword())) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Your password must be at least 8 characters long and contain at least one uppercase letter and one special character.");
+        }
+        String hashedPassword = passwordEncoder.encode(credentials.getPassword());
+        credentials.setPassword(hashedPassword);
+        userRepository.save(credentials);
+        return ResponseEntity.ok("User account created successfully!");
+    }
+
+    public ResponseEntity<?> signIn(@RequestBody User credentials) {
+        User existingUser = userRepository.findByUsername(credentials.getUsername());
+        if (existingUser != null && passwordEncoder.matches(credentials.getPassword(), existingUser.getPassword())) {
+            return ResponseEntity.ok("Login successful");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+    }
+
+    private boolean isValidPassword(String password) {
         if (!password.matches(".*[A-Z].*")) {
             return false;
         }
@@ -20,23 +48,5 @@ public class UserService {
             return false;
         }
         return true;
-    }
-    public ResponseEntity<?> signUp(User credentials) {
-        if (userRepository.existsByUsername(credentials.getUsername())) {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Username not available!");
-        }
-        if (!isValidPassword(credentials.getPassword())) {
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Your password must be at least 8 characters long and contain at least one uppercase letter and one special character.");
-        }
-        userRepository.save(credentials);
-        return ResponseEntity.ok("User account created successfully!");
-    }
-    public ResponseEntity<?> signIn(@RequestBody User credentials) {
-        User existingUser = userRepository.findByUsername(credentials.getUsername());
-        if (existingUser != null && credentials.getPassword().equals(existingUser.getPassword())) {
-            return ResponseEntity.ok("Login successful");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
-        }
     }
 }
